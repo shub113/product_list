2b8a6ad9b975b856b22d3a8440c0ffb6513987a2
shub113t <shubham@56secure.com>
2023-08-05 13:26:27 +0530

    Updates

--- assets/noPath-6150fe0d.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-3977a1d3.js";function r(){return e.jsx("div",{className:"grid min-h-screen place-items-center text-center",children:"No matching path"})}export{r as NoPath};
